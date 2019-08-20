e0ac8f72548f79581d58ce9174fdeb6bae919d54
Shahidul Islam Majumder <dev@shahid.pro>
2019-08-20 14:22:07 +0600

    Fix container styles

--- components/Container.js
+++ components/Container.js
@@ -23,9 +23,25 @@ const Wrapper = styled.div`
     font-size: 1em;
   }
 
+  img.small {
+    max-width: 70%;
+  }
+
+  ul {
+    text-align: left;
+
+    li {
+      margin-bottom: 16px;
+    }
+  }
+
   pre {
     border-radius: 4px;
     font-size: 0.9em;
+  }
+
+  code {
+    color: GREENYELLOW;
   }
 
   ${({ variant }) => {
